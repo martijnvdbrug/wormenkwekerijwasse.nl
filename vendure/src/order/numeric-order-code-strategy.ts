3f7c103303fc0eb08043b928ec16c222cd7ddce6
martijn <martijn.brug@gmail.com>
2021-03-04 13:48:18 +0100

    Shorter number

--- vendure/src/order/numeric-order-code-strategy.ts
+++ vendure/src/order/numeric-order-code-strategy.ts
@@ -2,12 +2,12 @@ import {OrderCodeStrategy, RequestContext} from '@vendure/core';
 import generate = require('nanoid/generate');
 
 /**
- * Generates id's like 20210304-0123456789
+ * Generates id's like 20210304-01234567
  */
 export class NumericOrderCodeStrategy implements OrderCodeStrategy {
     generate(ctx: RequestContext): string {
         const date = new Date();
-        const suffix = generate('1234567890', 10);
+        const suffix = generate('1234567890', 8);
         const dateString = date.getFullYear() + ('0' + (date.getMonth()+1)).slice(-2) + ('0' + date.getDate()).slice(-2);
         return `${dateString}-${suffix}`;
     }
