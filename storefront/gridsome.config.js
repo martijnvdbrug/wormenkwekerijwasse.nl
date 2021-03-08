38fa436730427e780675fbd4ade319bc7331fb80
martijn <martijn.brug@gmail.com>
2021-03-08 15:25:39 +0100

    Added sitemap generation

--- storefront/gridsome.config.js
+++ storefront/gridsome.config.js
@@ -5,19 +5,22 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Wormenkwekerij Wasse',
-  configureWebpack: {
-    resolve: {
-      symlinks: false //npm link
+    siteName: 'Wormenkwekerij Wasse',
+    siteUrl: 'https://wormenkwekerijwasse.nl',
+    configureWebpack: {
+        resolve: {
+            symlinks: false //npm link
+        },
     },
-  },
-  plugins: [
-    {
-      use: '@gridsome/source-graphql',
-      options: {
-        url: process.env.GRIDSOME_VENDURE_API,
-        fieldName: 'Vendure'
-      },
-    },
-  ],
+    plugins: [
+        {
+            use: '@gridsome/source-graphql',
+            options: {
+                url: process.env.GRIDSOME_VENDURE_API,
+                fieldName: 'Vendure'
+            }
+        }, {
+            use: '@gridsome/plugin-sitemap',
+        },
+    ],
 }
